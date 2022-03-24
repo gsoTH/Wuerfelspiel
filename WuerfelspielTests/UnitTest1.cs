4faf9caf9b0e524f8122132c4d2ce06832e92530
M.Thomas <th@gso-koeln.de>
2022-03-24 21:28:18 +0100

    Testprojekt hinzugefügt

---
+++ WuerfelspielTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WuerfelspielTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
